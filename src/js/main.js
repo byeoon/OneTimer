0f0486b592e6489aa17019ce031bd67794832b50
byron <47872200+byeoon@users.noreply.github.com>
2024-02-05 12:13:43 -0500

    added more placeholders

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
 
+
+const date = new Date();
+function updateMainClock()
+{
+    document.getElementById("time").textContent = date;
+}
 function getTime()
 {
     return new Date().getTime();;
+}
+
+function TimerSetup()
+{
+    alert("Working on the timer setup function, please wait.");
 }
