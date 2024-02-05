ffef954577b9656f110cef2008e85674d206597a
byron <47872200+byeoon@users.noreply.github.com>
2024-02-05 12:43:16 -0500

    center screen!!

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 function updateTime() {
     now = new Date();
-    document.getElementById("time").textContent = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+    document.getElementById("time").textContent = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds() + " ";
  }
 
- 
+
 function TimerSetup()
 {
     alert("Working on the timer setup function, please wait.");
