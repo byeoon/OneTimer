5ef5c4c8e61cb5c422abf80079c4b47e2deb5795
byron <47872200+byeoon@users.noreply.github.com>
2024-02-27 12:02:02 -0500

    FIxed batch scripts

--- start_host.bat
+++ start_host.bat
@@ -1,6 +1,9 @@
 @echo off 
-
 set dir=%cd%
 @echo %dir%
 
+FOR /F "tokens=4 delims= " %%i in ('route print ^| find " 0.0.0.0"') do set localIp=%%i
+
 http-server %dir% -p 8080
+echo The link to the local OnTimer server should be at %localIp%:8080 
+pause
