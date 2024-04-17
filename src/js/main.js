1c792c38e3be5a12f72a2d961f1783db52e31fda
byeoon <47872200+byeoon@users.noreply.github.com>
2024-04-17 15:50:29 +0000

    typo lmao

--- src/js/main.js
+++ src/js/main.js
@@ -51,8 +51,9 @@ function startTimer(duration) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        if(mode = "timer")
+        if(mode == "timer") {
             document.getElementById("time").textContent = minutes + ":" + seconds;
+        }
 
         if (--timer < 0) {
             timer = 0;
