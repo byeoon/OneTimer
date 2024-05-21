1bf5fc37842ce17f7153a260935558189e004527
byeoon <47872200+byeoon@users.noreply.github.com>
2024-05-21 13:37:33 -0400

    augh

--- src/js/main.js
+++ src/js/main.js
@@ -65,8 +65,8 @@ function createTimer()
 function startTimer(duration) {
     var timer = duration, hours, minutes, seconds;
     setInterval(function () {
-        hours = parseInt(timer / 3600);
-        timer %= 3600;
+        hours = parseInt(timer / 3600, 10);
+       // timer %= 3600;
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
