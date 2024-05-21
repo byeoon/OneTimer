e80edd4f787f52a1d8dbb04ee6ffd131ac7e4351
byeoon <47872200+byeoon@users.noreply.github.com>
2024-05-21 10:29:34 -0400

    this may not work

--- src/js/main.js
+++ src/js/main.js
@@ -47,15 +47,17 @@ function createTimer()
 }
 
 function startTimer(duration) {
-    var timer = duration, minutes, seconds;
+    var timer = duration, hours, minutes, seconds;
     setInterval(function () {
+        hours = parseInt(timer * 3600, 10)
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
+        hours = hours < 10 ? "0" + hours : hours;
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         document.getElementById("timerCURRENTTHING").style.visibility = "visible";
-        document.getElementById("timerCURRENTTHING").textContent = minutes + ":" + seconds;
+        document.getElementById("timerCURRENTTHING").textContent = hours + ":" + minutes + ":" + seconds;
        
 
         if (--timer < 0) {
